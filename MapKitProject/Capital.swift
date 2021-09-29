a91649ecdff22cc5ca2d1752d57e66cdfeb6fcff
NTatarnikov <t4t4rn1k0v@gmail.com>
2021-09-29 09:48:32 +0300

    Commit all changes and demonstration gif

---
+++ MapKitProject/Capital.swift
@@ -0,0 +1,22 @@
+//
+//  Capital.swift
+//  MapKitProject
+//
+//  Created by Никита on 28.09.2021.
+//
+
+import MapKit
+import UIKit
+
+class Capital: NSObject, MKAnnotation {
+    var title: String?
+    var coordinate: CLLocationCoordinate2D
+    var info: String
+    
+    init(title: String, coordinate: CLLocationCoordinate2D, info: String) {
+        self.title = title
+        self.coordinate = coordinate
+        self.info = info
+    }
+
+}
